98eb4f781388faf47d7d12e0ac5d35dc5ee0ca4e
anonfoxer <kevinandlife@gmail.com>
2020-05-07 13:12:32 -0500

    Update v2.0

--- Program.cs
+++ Program.cs
@@ -11,84 +11,62 @@ namespace WoTTool
     class Program
     {
 
-        static string rootFolder = @"C:\Games\World_of_Tanks\res_mods\1.6.0.4\"; //This is the directory im using for the mods deletion. This means I have to update it with game patches but Im still trying to figure out a way to go through each directory and then delete stuff. Oh well, working on that lol. Least I updated this project and didnt abadon it.
+        static string rootFolder = @"C:\Games\World_of_Tanks\res_mods\1.9.*.*\"; //This is the directory im using for the mods deletion. This means I have to update it with game patches but Im still trying to figure out a way to go through each directory and then delete stuff. Oh well, working on that lol. Least I updated this project and didnt abadon it.
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.WriteLine(@" 
- __      __            .__       .___         _____  ___________              __            
-/  \    /  \___________|  |    __| _/   _____/ ____\ \__    ___/____    ____ |  | __  ______
-\   \/\/   /  _ \_  __ \  |   / __ |   /  _ \   __\    |    |  \__  \  /    \|  |/ / /  ___/
- \        (  <_> )  | \/  |__/ /_/ |  (  <_> )  |      |    |   / __ \|   |  \    <  \___ \ 
-  \__/\  / \____/|__|  |____/\____ |   \____/|__|      |____|  (____  /___|  /__|_ \/____  >
-       \/                         \/                                \/     \/     \/     \/ 
-                           __________.__                                                    
-                           \______   \__|_____ _____________                                
-                            |       _/  \____ \\____ \_  __ \                               
-                            |    |   \  |  |_> >  |_> >  | \/                               
-                            |____|_  /__|   __/|   __/|__|                                  
-                                   \/   |__|   |__|                                         
+            Console.WriteLine(@"                                 
 - World of Tanks Rippr
-- v1.4.0
+- v2.0.0
 - by anonfoxer
 - (c) 2019
 - github.com/anonfoxer
 ");
             #region main
             Console.WriteLine("1. Clear all replay files"); //clearReplays();
-            Console.WriteLine("2. Verify all game files"); //verifyFiles();
-            Console.WriteLine("3. Clear all mods from res_mods"); //clearMods();
-            Console.WriteLine("4. Dump cef.log"); //cefDump();
-            Console.WriteLine("5. Remove stuff from Curse/Twitch Client"); //thisDoesntWork();
-            Console.WriteLine("6. Country Bias Finder"); //russianBias();
-            Console.WriteLine("7. Delete logs"); //freeSpace();
-            Console.WriteLine("8. Clear temp"); //templesS();
-            Console.WriteLine("9. Reset cef.log"); //logReset();
-            Console.WriteLine("10. Info"); //informMe();
+            Console.WriteLine("2. Clear all mods from res_mods"); //clearMods();
+            Console.WriteLine("3. Dump cef.log"); //cefDump();
+            Console.WriteLine("4. Country Bias Calculator"); //russianBias();
+            Console.WriteLine("5. Delete logs"); //freeSpace();
+            Console.WriteLine("6. Clear temp"); //templesS();
+            Console.WriteLine("7. Reset cef.log"); //logReset();
+            Console.WriteLine("8. Info/Changelog"); //informMe();
 
             #region choicehandle
 
             var choice = Console.ReadLine();
-            int bigChoice = Convert.ToInt32(choice);
-            if (bigChoice == 1)
+            int choiceSwitch = Convert.ToInt32(choice);
+            switch (choiceSwitch)
             {
-                clearReplays();
-            }
-            if (bigChoice == 2)
-            {
-                verifyFiles();
-            }
-            if (bigChoice == 3)
-            {
-                clearMods();
-            }
-            if (bigChoice == 4)
-            {
-                cefDump();
-            }
-            if (bigChoice == 5)
-            {
-                thisDoesntWork();
-            }
-            if (bigChoice == 6)
-            {
-                russianBias();
-            }
-            if (bigChoice == 7)
-            {
-                freeSpace();
-            }
-            if (bigChoice == 8)
-            {
-                templesS();
-            }
-            if (bigChoice == 9)
-            {
-                logReset();
-            }
-            if (bigChoice == 10)
-            {
-                informMe();
+                case 1:
+                    clearReplays();
+                    break;
+                case 2:
+                    clearMods();
+                    break;
+                case 3:
+                    cefDump();
+                    break;
+                case 4:
+                    russianBias();
+                    break;
+                case 5:
+                    freeSpace();
+                    break;
+                case 6:
+                    templesS();
+                    break;
+                case 7:
+                    logReset();
+                    break;
+                case 8:
+                    informMe();
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("No choice made.");
+                    Console.ReadLine();
+                    break;
             }
         }
         #endregion
@@ -104,20 +82,6 @@ namespace WoTTool
             }
             Console.WriteLine("Press any key to close.");
             Console.ReadLine();
-        }
-        public static void verifyFiles()
-        {
-            string[] files = System.IO.Directory.GetFiles(@"C:\Games\World_of_Tanks\", "*.*");
-            foreach (string file in files)
-            {
-                Console.WriteLine($"{file} is present!");
-                System.Threading.Thread.Sleep(300);
-                //Everything kinda closes like this
-                /* So a note to make about this is that this is not what i intend for this function to do.
-                 * yes this does verify that files are existant but does not verify that files are missing nor does it check integrity.
-                 * The hope is to keep a nice lil array or list or something of all the neccissarry files and directories that need to be there
-                 * and have the tool check for them. that'll hopefully come to fruition. */
-            }
         }
         public static void clearMods()
         {
@@ -144,10 +108,17 @@ namespace WoTTool
             Console.WriteLine("C:>Games>World_of_Tanks");
             Console.WriteLine("This is planned to be changed in future tool versions so the user can set.");
             Console.WriteLine("Updates available at:");
-            Console.WriteLine("anonhub.weebly.com");
             Console.WriteLine("github.com/anonfoxer");
-            Console.WriteLine("v1.4.0 - More features coming soon");
+            Console.WriteLine("------ CHANGELOG ------");
             Console.WriteLine(" ");
+            Console.WriteLine(@"                                 
+- v2.0.0
+- Removed Twitch/Curse client content removal.
+- Removed the file verifyer.
+- Cleaned up and optimized the code!
+- Added Changelog!
+- 
+");
             Console.WriteLine("Press any key to close.");
             Console.ReadLine();
         }
@@ -159,17 +130,6 @@ namespace WoTTool
             // Display the log contents to the console. Variable text is a string.
             System.Console.WriteLine("cef.log = {0}", log);
             Console.WriteLine(" ");
-            Console.WriteLine("Press any key to close.");
-            Console.ReadLine();
-        }
-        public static void thisDoesntWork()
-        {
-            string[] curseclient = System.IO.Directory.GetFiles(@"C:\Games\World_of_Tanks\", "*.curseclient");
-            foreach (string file in curseclient)
-            {
-                System.IO.File.Delete(file);
-                Console.WriteLine($"{file} was deleted");
-            }
             Console.WriteLine("Press any key to close.");
             Console.ReadLine();
         }
